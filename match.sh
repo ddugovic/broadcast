6cf5919e723c0f147997622b6ba8def6d1c40157
Daniel Dugovic <dandydand@gmail.com>
2017-12-28 18:51:20 -0600

    Rename 1.pgn to game-1.pgn

--- match.sh
+++ match.sh
@@ -1,1 +1,1 @@
-cutechess-cli -variant crazyhouse -openings file=crazyhouse.pgn format=pgn -engine cmd=stockfish proto=uci -engine cmd=stockfish proto=uci -each tc=240/10+0.1 -pgnout 1.pgn
+cutechess-cli -variant crazyhouse -openings file=crazyhouse.pgn format=pgn -engine cmd=stockfish proto=uci -engine cmd=stockfish proto=uci -each tc=240/10+0.1 -pgnout game-1.pgn
